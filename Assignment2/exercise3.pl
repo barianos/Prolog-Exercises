62b56616dd5f5e33d20131cd6729a200a54b5e82
Anastasios Barianos <barianos@outlook.com>
2018-03-25 18:34:38 +0300

    doc: added some documentantion

--- Assignment2/exercise3.pl
+++ Assignment2/exercise3.pl
@@ -1,6 +1,9 @@
+%% a predicate that will read the contents of the old knowledge base
+%% and will write them in a new file
 createnewKB:-
-  see('kb_old.pl'),
-  read(X),
-  tell('kb_new.pl'),
-  write(X),
-  (\+(X = end_of_file)-> createnewKB ; told, seen).
+  see('kb_old.pl'), %% the file we read from
+  read(X),  %%read the first part
+  tell('kb_new.pl'), %%the file we write to
+  write(X), %%write to the file
+  (\+(X = end_of_file)-> createnewKB ; told, seen). %%if the stream is not at the
+  %%end of the file we repeat, otherwise we close the two files.
