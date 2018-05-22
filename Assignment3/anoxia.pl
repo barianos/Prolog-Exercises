2a7a65f37ad2edfca81fa2876c16acb7a6d7d76a
Anastasios Barianos <barianos@outlook.com>
2018-05-23 00:52:47 +0300

    completed exercise

--- Assignment3/anoxia.pl
+++ Assignment3/anoxia.pl
@@ -1,34 +1,87 @@
-:-['anoxiaKB.pl'].
-
-anoxia(Result) :-
-  write('Do you have lab Data?(yes/no)'), %print message for user
-  nl,
-  read(Input),  %read input from user
-  nl,
-  anoxiaLabValues(Input,Result).
-  %anoxiaNoLabValues(Input,[Oligochaetes, Sediments, H2S, CH4, Substrate, Smell],Answer)),
-  %Result = Answer.
-  %bagof(Answer,rule(Input,[Oxygen,NO3,Oligochaetes, Sediments, H2S, CH4, Substrate, Smell], Aswer), Result).
+%% Advanced Topics in Artificial Intelligence
+%% TEI of Crete
+%% Barianos Kristofer Anastasios   MTP179
+%% Spring Semester 2018
 
 
-anoxiaLabValues(yes,Res):-
-  write('What is the percentage of oxygen?'),
-  nl,
-  read(Oxygen),
-  nl,
-  oxygenLow(Oxygen,NO3),
-  bagof(Answer,rule(data(yes,[Oxygen,NO3],Answer)),Res).
-  %Values = [Oxygen, NO3].
+%%A program that checks for anoxia levels in water
+%%Start the program by runing predicate \anoxia(Result).
 
-oxygenLow(Oxygen, NO3):-
-  number(Oxygen),
-  Oxygen < 20,
-  write('Is NO3 present? (yes/no)'),
+
+:-['anoxiaKB.pl']. %consult our knowledge base
+
+anoxia(Results) :-  %Predicate initiates all the program
+	write('Are there lab data?(yes/no)'), %ask the user
+	nl,
+	read(Data), %Input data
   nl,
+	(anoxia_labValues(Data, [Oxygen, NO3]) %data exists
+  ; anoxia_noLabValues(Data,[Oligochaetes,Sediments, H2S, CH4,Substrate,Smell])),  %data don't exist
+  %Search the kb for results
+	bagof(Answer, rule(data(Data,[Oxygen,NO3,Oligochaetes,Sediments,H2S, CH4,Substrate,Smell]),Answer),  Results).
+
+%predicate that is used when data exists:
+anoxia_labValues(yes, [Oxygen, No3]) :-
+	write('What is the percentage of Oxygen? (intiger number)'), %ask for the data
+  nl,
+	read(Oxygen),  %read the percentage of oxygen
+  number(Oxygen), %make sure that user did not imput invalid data
+  ( (Oxygen <20 , %if oxygen is less than 20%
+    no3values(NO3) ) ;  %ask for NO3 data
+  (Oxygen >= 20)).  % if oxygen is more than 20 we already arive at a leaf
+
+%predicate that asks for input of NO3 data
+no3values(NO3):-
+  write('Is there a presence of NO3?(yes/no)'),
+	nl,
   read(NO3),
   nl.
 
-oxygenLow(Oxygen,NO3):-
-  number(Oxygen),
-  Oxygen >=20,
-  NO3 = no.
+
+
+%predicate that is used when data does not exist
+anoxia_noLabValues(no,[Oligochaetes,Sediments,H2S, CH4,Substrate,Smell]):-
+  write('Are there red oligochaetes present? (yes/no/few)'),
+  nl,
+  read(Oligochaetes), %get oligochaetes input
+  nl,(
+      (
+				Oligochaetes = few, ! %if oligochaetes is few, stop
+			);
+			(
+				Oligochaetes = yes, ! %if oligochaetes exist, stop
+			);(
+      Oligochaetes = no, % if oligochaetes doesn't exist check for more
+      write('Is there a presence of sediments? (dark/black/light)'),
+      nl,
+      read(Sediments), %get sediments input
+      (
+      (Sediments = black ; Sediments = dark),   % if sediments are dark/black
+        write('Is H2S present? (yes/no)'),  %%check the H2S
+        nl,
+        read(H2S),
+        ((H2S = yes), ! ;     % if H2S is present we arive at another leaf
+        (H2S = no,            %if h2S is not present check further down the tree
+        write('Is CH4 present? (yes/no)'),
+        nl,
+        read(CH4),    %get CH4 presence
+        nl
+        ))
+      ;
+      (
+        Sediments = light,  %when sediment is light we have to check texture and smell
+        write('What is the texture of the substrate?(big/middle/fine)'),
+        nl,
+        read(Substrate),
+        nl,
+        (
+        (Substrate = big ; Substrate = middle), ! %%if texture is big or middle
+        );      %%we arived at a leaf and stop
+        (
+        write('Does the water smell bad? (yes,no)'),  %%otherwise we check the Smells of the water
+        nl,
+        read(Smell),
+        nl
+        )
+      )
+      ))).
--- Assignment3/anoxiaKB.pl
+++ Assignment3/anoxiaKB.pl
@@ -1,63 +1,74 @@
-rule(data(yes, [Oxygen,NO3], Answer)) :-
-  number(Oxygen),
-  Oxygen >=20,
-  Answer = 'No problem of anoxia'.
+%% Advanced Topics in Artificial Intelligence
+%% TEI of Crete
+%% Barianos Kristofer Anastasios   MTP179
+%% Spring Semester 2018
 
-rule(data(yes,[Oxygen,NO3], Answer)) :-
+
+%%Knowledge base for anoxia levels in water
+
+
+rule(data(yes, [Oxygen, NO3, Oligochaetes, Sediments, H2S, CH4, Sediments, Smells]),Answer):-
+	Oxygen >= 20,  
+	Answer = ' No Problem Of Anoxia'.
+
+rule(data(yes,[Oxygen, NO3, Oligochaetes, Sediments, H2S, CH4, Substrate, Smell]), Answer) :-
   number(Oxygen),
   Oxygen < 20,
-  presence(NO3),
+  NO3 = yes,
   Answer = 'Severe problem of anoxia'.
 
 
-rule(data(yes,[Oxygen,NO3], Answer)) :-
+
+rule(data(yes,[Oxygen, NO3, Oligochaetes, Sediments, H2S, CH4, Substrate, Smell]), Answer) :-
   Oxygen < 20,
-  \+ presence(NO3),
+  NO3 = no,
   Answer = 'Very severe problem of anoxia'.
 
-rule(data(no,[Oligochaetes, Sediments, H2S, CH4, Substrate, Smell], Answer)) :-
+rule(data(no,[Oxygen, NO3, Oligochaetes, Sediments, H2S, CH4, Substrate, Smell]), Answer) :-
   Oligochaetes = few,
   Answer = 'Very severe problem of anoxia'.
 
-rule(data(no,[Oligochaetes, Sediments, H2S, CH4, Substrate, Smell], Answer)) :-
+rule(data(no,[Oxygen, NO3, Oligochaetes, Sediments, H2S, CH4, Substrate, Smell]), Answer) :-
   Oligochaetes = yes,
   Answer = 'Moderate anoxia'.
 
-rule(data(no,[Oligochaetes, Sediments, H2S, CH4, Substrate, Smell], Answer)) :-
+rule(data(no,[Oxygen, NO3, Oligochaetes, Sediments, H2S, CH4, Substrate, Smell]), Answer) :-
   Oligochaetes = no,
   (Sediments = dark ; Sediments = black),
   H2S = yes,
   Answer = 'Very severe problem of anoxia'.
 
-rule(data(no,[Oligochaetes, Sediments, H2S, CH4, Substrate, Smell], Answer)) :-
+rule(data(no,[Oxygen, NO3, Oligochaetes, Sediments, H2S, CH4, Substrate, Smell]), Answer) :-
   Oligochaetes = no,
   (Sediments = dark ; Sediments = black),
   H2S = no,
-  CH3 = yes,
+  CH4 = yes,
   Answer = 'Very severe problem of anoxia'.
 
-rule(data(no,[Oligochaetes, Sediments, H2S, CH4, Substrate, Smell], Answer)) :-
+rule(data(no,[Oxygen, NO3, Oligochaetes, Sediments, H2S, CH4, Substrate, Smell]), Answer) :-
   Oligochaetes = no,
   (Sediments = dark ; Sediments = black),
   H2S = no,
-  CH3 = no,
+  CH4 = no,
   Answer = 'Severe problem of anoxia'.
 
 
-rule(data(no,[Oligochaetes, Sediments, H2S, CH4, Substrate, Smell], Answer)) :-
-  Oligochaetes = no,
-  Sediments = light,
-  (Substrate = middle ; Substrate = big),
+rule(data(no,[Oxygen, NO3, Oligochaetes, Sediments, H2S, CH4, Substrate, Smell]), Answer) :-
+  Oligochaetes = no,  %%if there are no oligochaetes
+  Sediments = light,  %% and sediment is ligt
+  (Substrate = middle ; Substrate = big), %% and the substrare is big or middle
+  Answer = 'No problem of anoxia'. %% there is no problem
+
+%%Ariving at the next two rules means that there are no oligochaetes,
+%%      the sediment is light and the substrate is fine
+%%      smell is only checked under these conditions so in order to save computations
+%%      we can check for the smell only, knowing the results will be right.
+%%However it is a shortcut that could bring us problems in larger programs
+
+rule(data(no,[Oxygen, NO3, Oligochaetes, Sediments, H2S, CH4, Substrate, Smell]), Answer) :-
+  Smell = no, %if the watet doenst smell then there is no anoxia
   Answer = 'No problem of anoxia'.
 
-rule(data(no,[Oligochaetes, Sediments, H2S, CH4, Substrate, Smell], Answer)) :-
-  Oligochaetes = no,
-  Sediments = fine,
-  Smell = no,
-  Answer = 'No problem of anoxia'.
-
-rule(data(no,[Oligochaetes, Sediments, H2S, CH4, Substrate, Smell], Answer)) :-
-  Oligochaetes = no,
-  Sediments = fine,
-  Smell = yes,
+rule(data(no,[Oxygen, NO3, Oligochaetes, Sediments, H2S, CH4, Substrate, Smell]), Answer) :-
+  Smell = yes, %if the water smells there is modeate anoxia
   Answer = 'Moderate anoxia'.
